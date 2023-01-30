a6f9ba09c10bba679676d1b88324f9f0838f840f
maSemeao <maurilio.ssemeao@gmail.com>
2023-01-29 21:49:22 -0300

    :construction: Create HTML of the PlayerScore component

---
+++ src/components/PlayerScore/index.ts
@@ -0,0 +1,13 @@
+
+function PlayerScore(points:number = 0) {
+    
+    return /* html */`
+        <ol data-points="${points}" >
+            <li>Um</l1>
+            <li>Dois</l1>
+            <li>Tres</l1>
+        </ol>
+    `;
+}
+
+export default PlayerScore;
--- src/objects/ScoreBoard/index.ts
+++ src/objects/ScoreBoard/index.ts
@@ -1,15 +1,18 @@
 import "./style.css"
 import PlayerName from "../../components/PlayerName";
 import VsPlayer from "../../components/VsPlayer";
+import PlayerScore from "../../components/PlayerScore";
 
 function ScoreBoard() {
     return /*html*/`
         <header class="score-board">
             ${PlayerName("Poayer1")}
+            ${PlayerScore()}
             ${VsPlayer()}
+            ${PlayerScore()}
             ${PlayerName("Player2")}
         </header> 
-    `
+    `;
 }
 
 export default ScoreBoard;
