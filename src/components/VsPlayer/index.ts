51c53a9f76f0b6dd239ed399536dea41a71b0a82
maSemeao <maurilio.ssemeao@gmail.com>
2023-01-29 21:17:48 -0300

    :construction: Create the VsPlayer component without CSS

---
+++ src/components/VsPlayer/index.ts
@@ -0,0 +1,8 @@
+
+function VsPlayer(content: string= "vs") {
+    return /* html */`
+        <span class="vs-player" >${content}</span>
+    `
+}
+
+export default VsPlayer;
--- src/objects/ScoreBoard/index.ts
+++ src/objects/ScoreBoard/index.ts
@@ -1,10 +1,12 @@
 import "./style.css"
 import PlayerName from "../../components/PlayerName";
+import VsPlayer from "../../components/VsPlayer";
 
 function ScoreBoard() {
     return /*html*/`
         <header class="score-board">
             ${PlayerName("Poayer1")}
+            ${VsPlayer()}
             ${PlayerName("Player2")}
         </header> 
     `
