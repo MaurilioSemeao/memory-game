d96ccef9cb434fedb106efe0488c1e5ffba65c70
maSemeao <maurilio.ssemeao@gmail.com>
2023-01-29 23:02:19 -0300

    :construction: Create ArrowDown component without CSS

---
+++ src/components/ArrowDown/index.ts
@@ -0,0 +1,10 @@
+
+function ArrowDown():string {
+    
+    return /* html */`
+        <img src="../../images/IconArrowDown.png" alt=" icone de uma seta para baixo" >    
+    `;
+
+}
+
+export default ArrowDown;
